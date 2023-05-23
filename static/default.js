b1873e358d1feddd1a49c8ad637adeee6d2cb3e8
mahfuz225bd <mahfuz225bd@gmail.com>
2023-05-23 12:05:25 +0600

    checkout product list is done

--- static/default.js
+++ static/default.js
@@ -15,4 +15,31 @@ document.addEventListener('DOMContentLoaded', () => {
             modal.style.display = "none";
         }
     }
+
+    // Checkout
+    const tempProductListLi = (productName, unitPrice, qty, price) => `<li>
+    <strong>${productName}</strong>
+    <ul>
+        <li>Unit Price: ${unitPrice} &times; ${qty}
+        </li>
+        <li>Price: &#2547; ${price}</li>
+    </ul>
+</li>`
+
+    const cartItemsTbody = document.querySelector("#cartItems tbody")
+    const checkoutTr = cartItemsTbody.querySelectorAll('tr');
+    let totalPrice = 0
+    checkoutTr.forEach(each => {
+        const col1 = each.querySelectorAll('td')[2].innerText
+        const col2 = each.querySelectorAll('td')[3].innerText
+        const col3 = each.querySelectorAll('td')[4].querySelector('input[type="number"]').value
+        const col4 = each.querySelectorAll('td')[5].querySelector('.price').innerText
+        totalPrice += parseFloat(col4)
+
+        if (document.querySelector("#productList")) {
+            document.querySelector("#productList").innerHTML += tempProductListLi(col1, col2, col3, col4)
+        }
+    })
+
+    document.querySelector("#totalToCheckout").innerHTML = parseFloat(totalPrice)
 })
