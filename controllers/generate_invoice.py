e91fefff19ca36c96657c5bf8517e0fafa50380c
mahfuz225bd <mahfuz225bd@gmail.com>
2023-06-04 23:28:05 +0600

    generating invoice using existing .docx template as .docx (incomplete)

--- controllers/generate_invoice.py
+++ controllers/generate_invoice.py
@@ -1,23 +1,21 @@
 from docxtpl import DocxTemplate
-import comtypes.client
-from os import path
+import os
 
-# temp_invoice = DocxTemplate('../assets/Invoice Template.docx')
+target_dir = os.path.abspath('../assets')
 
-# # Replace all placeholders with new data
-# context = { 'payment_status' : "Unpaid" }
-# temp_invoice.render(context)
+# # Check if ~$ or .DS_ were not found
+# for file in os.listdir(target_dir):
+#     if file.startswith('~$') or file.startswith('.DS_'):
+#         raise Exception("Could not generate latest_invoice.docx because ~$ or .DS_ file were found.")
+    
+temp_invoice = DocxTemplate(os.path.abspath(target_dir) + '\Invoice Template.docx')
 
-# # Save as .docx
-# output_path_for_docx = '../assets/latest_invoice.docx'
-# temp_invoice.save(output_path_for_docx)
+# Replace all placeholders with new data
+context = { 'total' : "1" }
+temp_invoice.render(context)
 
-# Convert .docx to pdf
-in_file = path.abspath('./assets/latest_invoice.docx')
-out_file = path.abspath('./assets/latest_invoice.pdf')
+# Save as .docx
+output_path_for_docx = os.path.abspath(target_dir) + '\latest_invoice.docx'
+temp_invoice.save(output_path_for_docx)
 
-word = comtypes.client.CreateObject('Word.Application')
-doc = word.Documents.Open(in_file)
-doc.SaveAs(out_file, FileFormat=17)
-doc.Close()
-word.Quit()
+
